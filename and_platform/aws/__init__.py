c7b1c25fec0c0d623453e41a4ad5829e296574e1
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-10-26 04:43:25 +0700

    fix split

--- and_platform/aws/__init__.py
+++ and_platform/aws/__init__.py
@@ -3,17 +3,14 @@ import boto3
 import json
 from and_platform.core.config import get_config
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 AWS_REGION_NAME = os.getenv('AWS_REGION_NAME', 'ap-southeast-1')
 AWS_STACK_PREFIX = os.getenv('AWS_STACK_PREFIX', 'lks-warmup')
 
-GLOBAL_TEMPLATE_PARAMETERS = {
-    'Vpc': os.getenv('VPC_ID', 'vpcid'),
-    'PublicRouteTable': os.getenv('PUBLIC_ROUTE_TABLE', 'routetable'),
-    'EnvironmentName': AWS_STACK_PREFIX,
-    'FlagServerHost': os.getenv('FLAG_SERVER_HOST', 'https://flaggy.free.beeceptor.com'),
+stack_suffixes = {
+    0: 'global',
+    1: 'team',
 }
+
 
 TEAM_TEMPLATE_PARAMETERS = {
     'EnvironmentName': AWS_STACK_PREFIX,
@@ -21,21 +18,36 @@ TEAM_TEMPLATE_PARAMETERS = {
     'CTFMachineAMI2': os.getenv('AMI_ID2', 'CTFMachineAMI2'),
 }
 
-stack_suffixes = {
-    0: 'global',
-    1: 'team',
-}
+def get_global_template_params(team_id):
+    resp = {
+        'EnvironmentName': AWS_STACK_PREFIX,
+        'FlagServerHost': os.getenv('FLAG_SERVER_HOST', 'https://flaggy.free.beeceptor.com'),
+    }
+    
+    if team_id <= 8:
+        resp['Vpc'] = os.getenv('VPC_ID1', 'vpcid')
+        resp['PublicRouteTable'] = os.getenv('PUBLIC_ROUTE_TABLE1', 'routetable')
+    return resp
 
-aws_session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION_NAME,
-)
+def get_aws_session(team_id):
+    if team_id <= 8: grup = "1"
+    else: grup = "2"
+    
+    AWS_ACCESS_KEY_ID = os.getenv(f'AWS_ACCESS_KEY_ID{grup}', '')
+    AWS_SECRET_ACCESS_KEY = os.getenv(f'AWS_SECRET_ACCESS_KEY{grup}', '')
+    # print(AWS_ACCESS_KEY_ID)
+    
+    return boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION_NAME,
+    )
 
-cf_client = aws_session.client('cloudformation')
+def get_cf_client(team_id):
+    return get_aws_session(team_id).client('cloudformation')
 
-def create_key_pair(name):
-    ec2_client = aws_session.client('ec2')
+def create_key_pair(name, team_id = -1):
+    ec2_client = get_aws_session(team_id).client('ec2')
     try:
         response = ec2_client.create_key_pair(KeyName=name)
         return response['KeyMaterial']
@@ -49,22 +61,21 @@ def get_stack_name(stack_type, suffix=''):
         return '%s-%s-%s' % (AWS_STACK_PREFIX, stack_suffixes[stack_type], suffix)
     return '%s-%s' % (AWS_STACK_PREFIX, stack_suffixes[stack_type])
 
-def deploy_stack(stack_name, template_body, params):
+def deploy_stack(stack_name, template_body, params, team_id=-1):
     parameters = [{'ParameterKey': x, 'ParameterValue': params[x]} for x in params.keys()]
-
     print('Deploying stack %s' % stack_name)
 
     stack_exists = False
     try:
-        cf_client.describe_stacks(StackName=stack_name)
+        get_cf_client(team_id).describe_stacks(StackName=stack_name)
         stack_exists = True
-    except cf_client.exceptions.ClientError as e:
+    except get_cf_client(team_id).exceptions.ClientError as e:
         stack_exists = False
     
     try:
         if stack_exists:
             print('Stack exists, updating...')
-            cf_client.update_stack(
+            get_cf_client(team_id).update_stack(
                 StackName=stack_name,
                 TemplateBody=template_body,
                 Capabilities=['CAPABILITY_AUTO_EXPAND', 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
@@ -72,9 +83,9 @@ def deploy_stack(stack_name, template_body, params):
             )
             
             print('Waiting until stack %s deployed...' % stack_name)
-            cf_client.get_waiter('stack_update_complete').wait(StackName=stack_name)
+            get_cf_client(team_id).get_waiter('stack_update_complete').wait(StackName=stack_name)
             
-            stack_info = cf_client.describe_stacks(StackName=stack_name)
+            stack_info = get_cf_client(team_id).describe_stacks(StackName=stack_name)
             if stack_info['Stacks'][0]['StackStatus'] == 'UPDATE_COMPLETE':
                 print(f'Stack {stack_name} updated successfully!')
                 return stack_info['Stacks'][0]['Outputs']
@@ -82,7 +93,7 @@ def deploy_stack(stack_name, template_body, params):
                 print(f'Stack update failed. Status: {stack_info["Stacks"][0]["StackStatus"]}')
         else:
             print('Stack not exists, creating...')
-            cf_client.create_stack(
+            get_cf_client(team_id).create_stack(
                 StackName=stack_name,
                 TemplateBody=template_body,
                 Capabilities=['CAPABILITY_AUTO_EXPAND', 'CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
@@ -90,9 +101,9 @@ def deploy_stack(stack_name, template_body, params):
             )
 
             print('Waiting until stack %s deployed...' % stack_name)
-            cf_client.get_waiter('stack_create_complete').wait(StackName=stack_name)
+            get_cf_client(team_id).get_waiter('stack_create_complete').wait(StackName=stack_name)
             
-            stack_info = cf_client.describe_stacks(StackName=stack_name)
+            stack_info = get_cf_client(team_id).describe_stacks(StackName=stack_name)
             if stack_info['Stacks'][0]['StackStatus'] == 'CREATE_COMPLETE':
                 print(f'Stack {stack_name} created successfully!')
                 return stack_info['Stacks'][0]['Outputs']
@@ -103,25 +114,25 @@ def deploy_stack(stack_name, template_body, params):
         print(f'Stack creation failed. Status: %s' % e)
         return None
 
-def delete_stack(stack_name):
+def delete_stack(stack_name, team_id=-1):
     try:
-        cf_client.delete_stack(StackName=stack_name)
+        get_cf_client(team_id).delete_stack(StackName=stack_name)
         print(f'Stack {stack_name} deletion initiated.')
-    except cf_client.exceptions.ClientError as e:
+    except get_cf_client(team_id).exceptions.ClientError as e:
         if 'does not exist' in str(e):
             print(f'Stack {stack_name} does not exist.')
         return
 
     print('Waiting until stack %s deleted...' % stack_name)
-    cf_client.get_waiter('stack_delete_complete').wait(StackName=stack_name)
+    get_cf_client(team_id).get_waiter('stack_delete_complete').wait(StackName=stack_name)
 
     try:
-        stack_info = cf_client.describe_stacks(StackName=stack_name)
+        stack_info = get_cf_client(team_id).describe_stacks(StackName=stack_name)
         if stack_info['Stacks'][0]['StackStatus'] == 'DELETE_COMPLETE':
             print(f'Stack {stack_name} deleted successfully!')
         else:
             print(f'Stack deletion failed. Status: {stack_info["Stacks"][0]["StackStatus"]}')
-    except cf_client.exceptions.ClientError as e:
+    except get_cf_client(team_id).exceptions.ClientError as e:
         if 'does not exist' in str(e):
             print(f'Stack {stack_name} deleted successfully!')
         else:
@@ -130,10 +141,10 @@ def delete_stack(stack_name):
 def rollback(team_id):
     stack_name = get_stack_name(1, 'team' + str(team_id))
     
-    response = cf_client.describe_stacks(StackName=stack_name)
+    response = get_cf_client(team_id).describe_stacks(StackName=stack_name)
     stack_parameters = response['Stacks'][0]['Parameters']
 
-    response = cf_client.get_template(StackName=stack_name)
+    response = get_cf_client(team_id).get_template(StackName=stack_name)
     template_body = response['TemplateBody']
     if template_body['Resources']['CTFMachineEC2Instance']['Properties']['ImageId']['Ref'] == get_config('AMI_ID1', 'CTFMachineAMI1'):
         template_body['Resources']['CTFMachineEC2Instance']['Properties']['ImageId']['Ref'] = get_config('AMI_ID2', 'CTFMachineAMI2')
--- and_platform/core/server.py
+++ and_platform/core/server.py
@@ -1,5 +1,5 @@
 import celery
-from and_platform.aws import GLOBAL_TEMPLATE_PARAMETERS, TEAM_TEMPLATE_PARAMETERS, AWS_STACK_PREFIX, deploy_stack, get_stack_name, create_key_pair, rollback, delete_stack
+from and_platform.aws import get_global_template_params, TEAM_TEMPLATE_PARAMETERS, AWS_STACK_PREFIX, deploy_stack, get_stack_name, create_key_pair, rollback, delete_stack
 from and_platform.core.config import get_app_config, get_config, set_config
 import os
 import json
@@ -26,20 +26,21 @@ def replace_template(content, team_id = 0):
     content = content.replace("__TEAM__", f'team{team_id}')
     return content
 
-def provision_or_get_global_stack(is_force=False, fname="global.yml"):
+def provision_or_get_global_stack(is_force=False, fname="global.yml", team_id=-1):
     template_dir = os.path.join(get_app_config("TEMPLATE_DIR"), "server")
 
     with open(os.path.join(template_dir, fname)) as f:
         stack_global_template = f.read()
         stack_global_template = replace_template(stack_global_template)
 
-    global_template_params = GLOBAL_TEMPLATE_PARAMETERS
+    global_template_params = get_global_template_params(team_id)
     global_template_params["GateFlagSecret"] = "'" + get_config("GATEFLAG_SECRET") + "'"
-
+    # print(fname)
     output_stack_global = deploy_stack(
         get_stack_name(0),
         stack_global_template,
         global_template_params,
+        team_id=team_id,
     )
     
     output_stack_global_json = {}
@@ -48,8 +49,8 @@ def provision_or_get_global_stack(is_force=False, fname="global.yml"):
     return output_stack_global_json
 
 def do_server_bulk_provision(is_force):
-    for i in range(1, 3):
-        provision_or_get_global_stack(is_force, "global.yml." + str(i))
+    provision_or_get_global_stack(is_force, "global.yml.1", 1)
+    provision_or_get_global_stack(is_force, "global.yml.2", 16)
     teams = Teams.query.all()
     for team in teams:
         do_server_bulk_provision.apply_async(team.id)
@@ -58,7 +59,7 @@ def do_server_bulk_provision(is_force):
 def do_server_provision(team_id):
     template_dir = os.path.join(get_app_config("TEMPLATE_DIR"), "server")
     
-    output_stack_global = provision_or_get_global_stack(fname=get_template_filename("global.yml", team_id))
+    output_stack_global = provision_or_get_global_stack(fname=get_template_filename("global.yml", team_id), team_id=team_id)
 
     team_parameters = TEAM_TEMPLATE_PARAMETERS
     team_parameters.update(output_stack_global)
@@ -67,7 +68,7 @@ def do_server_provision(team_id):
     team_parameters['CTFEC2KeyPair'] = team_keypairname
     team_parameters['PrivateIpAddress'] = generate_team_private_ip(team_id)
 
-    ssh_privkey = create_key_pair(team_keypairname)
+    ssh_privkey = create_key_pair(team_keypairname, team_id)
     with open(os.path.join(template_dir, get_template_filename("team.yml", team_id))) as f:
         stack_team_template = f.read()
         stack_team_template = replace_template(stack_team_template, team_id)
@@ -76,6 +77,7 @@ def do_server_provision(team_id):
         get_stack_name(1, f'team{team_id}'),
         stack_team_template,
         team_parameters,
+        team_id=team_id
     )
         
     result_stack = {}
@@ -117,7 +119,7 @@ def do_server_bulk_destroy():
 @celery.shared_task
 def do_server_destroy(team_id):
     stack_name = get_stack_name(1, 'team' + str(team_id))
-    delete_stack(stack_name)
+    delete_stack(stack_name, team_id)
 
 @celery.shared_task
 def do_rollback(team_id):
