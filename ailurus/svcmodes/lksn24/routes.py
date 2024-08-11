5434906a412edada7bc542887d657e78d6ca9fea
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-08-11 18:59:59 +0700

    Implement checker agent report endpoint

--- ailurus/svcmodes/lksn24/routes.py
+++ ailurus/svcmodes/lksn24/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, request
+from ailurus.models import db
+from ailurus.utils.config import get_config
+from flask import Blueprint, request, jsonify
+
+from .models import CheckerAgentReport
+
+import json
+import jwt
 
 checker_agent_blueprint = Blueprint("checker_agent", __name__, url_prefix="/api/v2/checkeragent")
 
@@ -8,5 +15,28 @@ def receive_checker_agent_report():
         source_ip = request.headers.getlist('X-Forwarded-For')[0]
     else:
         source_ip = request.remote_addr
-    
-    # TODO: complete this
+    request_data = request.get_json().get("data")
+    if not request_data:
+        return jsonify(status="failed", message="missing required field.")
+
+    try:
+        claim = jwt.decode(request_data, get_config("CHECKER_AGENT_SECRET"), algorithms="HS256", leeway=5, options={"verify_iat": True, "verify_signature": True})
+        
+        report = CheckerAgentReport(
+            source_ip=source_ip,
+            selinux_status=claim["selinux"],
+            flag_status=json.dumps(claim["flag"]),
+            challenge_status=json.dumps(claim["chall"]),
+        )
+        db.session.add(report)
+        db.session.commit()
+
+        return jsonify(status="success")
+    except jwt.InvalidSignatureError:
+        return jsonify(status="failed", message="Invalid signature."), 400
+    except jwt.InvalidIssuedAtError:
+        return jsonify(status="failed", message="Token has expired."), 400
+    except jwt.ExpiredSignatureError:
+        return jsonify(status="failed", message="Token has expired."), 400
+    except Exception:
+        return jsonify(status="failed", message="invalid token."), 400
